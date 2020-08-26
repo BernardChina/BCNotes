08fa0a1433f3d5aa9c0a2178df2a737921ea7263
liuyongqiang <1054462882@qq.com>
2020-08-26 18:31:22 +0800

    hhs

--- notepassword/AppDelegate.m
+++ notepassword/AppDelegate.m
@@ -21,11 +21,16 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
+
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
     MainViewController *mainController = [[MainViewController alloc] init];
     UINavigationController *controller = [[UINavigationController alloc] initWithRootViewController:mainController];
     self.window.rootViewController = controller;
+//1
+    //2
+    //3
+    //4
     
     [self.window makeKeyAndVisible];
     
