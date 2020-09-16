0b78889a16683e08f531c840f67a05b2a7dba805
liuyongqiang <1054462882@qq.com>
2020-09-16 16:52:46 +0800

    3

--- notepassword/AppDelegate.m
+++ notepassword/AppDelegate.m
@@ -27,7 +27,7 @@
     MainViewController *mainController = [[MainViewController alloc] init];
     UINavigationController *controller = [[UINavigationController alloc] initWithRootViewController:mainController];
     self.window.rootViewController = controller;
-    //1,2
+    //1,2，3
 
     [self.window makeKeyAndVisible];
     
